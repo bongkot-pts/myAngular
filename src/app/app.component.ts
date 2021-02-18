e91c5fecc0e9951aeda35da2b0816d1d70ef08d0
Bongburin <bkittipong@yahoo.com>
2021-02-18 16:33:34 +0700

    2021-02-18 ver.01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myAngular';
+  name: string;
+  setName(name: string) {
+    this.name = name;
+  }
 }
---
+++ src/app/child-component/child-component.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-child-component',
+  templateUrl: './child-component.component.html',
+  styleUrls: ['./child-component.component.css']
+})
+export class ChildComponentComponent implements OnInit {
+
+  constructor() { }
+
+  @Output() inputValueFromChildX: EventEmitter<string> = new EventEmitter<string>();
+  ngOnInit() {
+  }
+
+  emitValueX(value: string) {
+    this.inputValueFromChildX.emit(value);
+  }
+}
